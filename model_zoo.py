6d893f0ca9866f56f08f0c0648f403e23473bdd8
Ethan <ethanabrooks@gmail.com>
2017-03-05 23:14:32 -0500

    figured out that layers are not being reused

--- model_zoo.py
+++ model_zoo.py
@@ -3,34 +3,39 @@ from operator import mul
 import tensorflow as tf
 
 
-def mlp(x, out_size, hidden_sizes=None):
+def mlp(x, out_size, hidden_sizes=None, reuse=False):
     if hidden_sizes is None:
         hidden_sizes = []
-    for size in hidden_sizes:
-        x = tf.layers.dense(x, size, activation=tf.sigmoid)
-    return tf.layers.dense(x, out_size)
+    for i, size in enumerate(hidden_sizes):
+        x = tf.contrib.fully_connected(x, size,
+                                       activation=tf.sigmoid,
+                                       scope="layer" + str(i),
+                                       reuse=reuse)
+    return tf.contrib.fully_connected(x, out_size,
+                                      scope="out_layer",
+                                      reuse=reuse)
 
 
 def maxpool2d(x, k=2):
     return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME')
 
 
-def conv_layer(i, x, filter_size, num_filters, stride=1):
+def conv_layer(i, x, filter_size, num_filters, stride=1, reuse=False):
     _, _, _, in_channels = x.get_shape()
     filter = tf.get_variable('filter' + str(i),
                              [filter_size, filter_size, in_channels, num_filters],
                              x.dtype)
-    return tf.nn.conv2d(x, filter, strides=[1, stride, stride, 1], padding='SAME')
+    return tf.nn.conv2d(x, filter, strides=[1, stride, stride, 1], padding='SAME', reuse=reuse)
 
 
-def conv_net(x, out_size, strides, filters_per_layer, filter_size_list, dense_size):
+def conv_net(x, out_size, strides, filters_per_layer, filter_size_list, dense_size, reuse=False):
     for i, (filter_size,
             num_filters,
             stride) in enumerate(zip(filter_size_list,
                                      filters_per_layer,
                                      strides)):
         conv_output = conv_layer(i, x, filter_size, num_filters, stride)
-        bias = tf.get_variable('bias' + str(i), num_filters)
+        bias = tf.get_variable('bias' + str(i), num_filters, reuse=reuse)
         x = tf.nn.relu(tf.nn.bias_add(conv_output, bias))
 
     return mlp(tf.reshape(x, [1, -1]), out_size, hidden_sizes=[dense_size])
--- q_learning.py
+++ q_learning.py
@@ -14,17 +14,16 @@ def gather_1d(params, indices):
 
 
 # noinspection PyPep8Naming
-def train(env, network, optimizer):
+def train(env, network, optimizer,
+          bsize=32,
+          epochs=50000,
+          gamma=.9, ):
     assert (isinstance(env.action_space, gym.spaces.Discrete))
     obs_size, = env.observation_space.shape
     act_size = env.action_space.n
     dtype = get_dtype(env)
-
-    bsize = 32
-    memory_buffer_size = 3 * bsize
-    epochs = 50000
-    gamma = .9
-    epsilon = .5
+    memory_buffer_size = 10000
+    epsilon = 1
 
     observation_ph = tf.placeholder(dtype, [1, obs_size], name='observation')
     tf_Q = network(observation_ph, act_size)
@@ -33,16 +32,21 @@ def train(env, network, optimizer):
     observations_ph = []
     actions_ph = tf.placeholder(tf.int32, [bsize], name='batch_actions')
     rewards_ph = tf.placeholder(dtype, [bsize], name='reward')
+    done_ph = tf.placeholder(tf.bool, [bsize], name='done')
     Qs = []
     for _ in range(2):
         batch_observations = tf.placeholder(dtype, [bsize, obs_size],
                                             name='batch_observations')
         observations_ph.append(batch_observations)
-        Qs.append(network(batch_observations, act_size))
+        Qs.append(network(batch_observations, act_size, reuse=True))
 
-    y = rewards_ph + gamma * tf.reduce_max(Qs[1], axis=1)
-    d = gather_1d(Qs[0], actions_ph)
-    tf_loss = tf.square(y - d)
+    y = tf.where(
+        done_ph,
+        rewards_ph,
+        rewards_ph + gamma * tf.reduce_max(Qs[1], axis=1)
+    )
+    y_guess = gather_1d(Qs[0], actions_ph)
+    tf_loss = tf.square(y - y_guess)
     train_op = optimizer.minimize(tf_loss)
 
     show_off = False
@@ -50,14 +54,16 @@ def train(env, network, optimizer):
         tf.global_variables_initializer().run()
         memory_buffer = []
 
+        step = 0
         # update every epoch
         for e in range(epochs):
-            mean_reward = 0
+            cumulative_reward = 0
+            cumulative_Q = 0
             observation = env.reset()
             done = False
 
-            step = 0
             # steps
+            memory_buffer_full = False
             while not done:
                 step += 1
                 if show_off:
@@ -67,24 +73,50 @@ def train(env, network, optimizer):
                 else:
                     action, Q = sess.run([nominal_action, tf_Q],
                                          {observation_ph: observation.reshape(1, -1)})
+                    cumulative_Q += Q[0, action]
 
+                if epsilon > .1:
+                    epsilon *= .1 ** (1. / 100000.)
+                    show_off = True
                 next_observation, reward, done, info = env.step(action)
-                if done:
-                    next_observation = None
-                mean_reward = mean_reward * (step - 1) / step + reward / step
-                memory_buffer.append((observation, action, reward, next_observation))
+                cumulative_reward += reward
+
+                memory_buffer.append((observation, action, reward, next_observation, done))
                 observation = next_observation
 
-                if len(memory_buffer) >= memory_buffer_size:
-                    random.shuffle(memory_buffer)
-                    batch, memory_buffer = memory_buffer[:bsize], memory_buffer[bsize:]
-                    sequence = zip(*batch)
-                    observations, actions, rewards, next_observations = map(np.array, sequence)
-                    _, loss = sess.run([train_op, tf_loss], feed_dict={
+                memory_buffer_full = len(memory_buffer) >= memory_buffer_size
+                if memory_buffer_full:
+                    memory_buffer.pop(0)
+                    batch = zip(*[random.choice(memory_buffer) for _ in range(bsize)])
+                    observations, actions, rewards, next_observations, done_values = batch
+                    _, loss, y_, y_guess_, Qs0, Qs1 = sess.run([train_op, tf_loss, y, y_guess, Qs[0], Qs[1]], feed_dict={
                         observations_ph[0]: observations,
                         actions_ph: actions,
                         rewards_ph: rewards,
                         observations_ph[1]: next_observations,
+                        done_ph: done_values,
                     })
 
-                    # print("Epoch: {}. Reward: {}. loss: {}".format(e, mean_reward, loss.mean()))
+            if memory_buffer_full and e % 200 == 0:
+                    # any(done_values):
+            # print(guess[np.stack([done_values, range(len(done_values))])])
+                print("Epoch: {}. Reward: {}. q: {}".format(e, cumulative_reward, cumulative_Q))
+                print(epsilon)
+                # print(loss.mean())
+                # print("y")
+                # print(y_)
+                # print(done_values)
+                # print('rewards')
+                # print(rewards)
+                # print('Qs[1]')
+                # print(Qs1)
+                # print('Qs[0]')
+                # print(Qs0)
+                # print('actions')
+                # print(actions)
+                # print('observation')
+                # print(observations)
+                # print("next_observation")
+                # print(next_observations)
+                # print(y_[[done_values]])
+                # print(y_guess_[[done_values]])
